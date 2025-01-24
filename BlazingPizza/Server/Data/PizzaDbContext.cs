01c875a2a426caa3cb3149dfed5daa0afecd0708
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-23 23:34:55 -0800

    add(PizzaDbContext.cs): Se agrega el contexto de base de datos para manejar pizzas

---
+++ BlazingPizza/Server/Data/PizzaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazingPizza.Server.Models;
+using System.Collections.Generic;
+
+namespace BlazingPizza.Server.Data
+{
+    public class PizzaDbContext : DbContext
+    {
+        public PizzaDbContext(DbContextOptions<PizzaDbContext> options) : base(options) { }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
